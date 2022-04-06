408d0e3b36c8e74e863ca5db65ceb219494af1c3
mia <90076@naver.com>
2022-04-06 22:30:42 +0900

    mainpage

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const filterBtn = document.querySelector("#filter");
-const searchBtn = document.querySelector("#direct");
+const filterBtn = document.querySelector("#filter-search");
+const searchBtn = document.querySelector("#direct-search");
 
-const searchText = document.querySelector(".change1");
-const searchFilters = document.querySelector(".change2");
+const searchText = document.querySelector(".func-direct-search");
+const searchFilters = document.querySelector(".func-filter-search");
 
 filterBtn.addEventListener("click", () => {
   if (searchText.classList.contains("show")) {
@@ -30,37 +30,37 @@ searchBtn.addEventListener("click", () => {
 
 //////////////////////////////////////////////////////////
 
-const searchClickBtn = document.querySelector("#click1");
-let imgContainer = document.querySelector(".screen");
-searchClickBtn.addEventListener("click", () => {
-  const word = document.querySelector("#searchText").value;
-  fetch("http://localhost:8080/search", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ word: word }),
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      imgContainer.innerHTML = "";
-      const images = json.images;
-      console.log(images);
-      // imgContainer.innerHTML = "";
-      for (let i = 0; i < images.length; i++) {
-        if (images[i] === null) {
-          continue;
-        }
-        const new_pTag1 = document.createElement("div");
-        new_pTag1.setAttribute("class", "movie1");
-        const new_pTag2 = document.createElement("img");
-        new_pTag2.setAttribute("class", "imgs");
-        new_pTag2.setAttribute("src", `${images[i]}`);
-        new_pTag2.setAttribute("alt", "movie1");
-        new_pTag1.appendChild(new_pTag2);
-        imgContainer.appendChild(new_pTag1);
-      }
-    });
-});
+// const searchClickBtn = document.querySelector("#click1");
+// let imgContainer = document.querySelector(".screen");
+// searchClickBtn.addEventListener("click", () => {
+//   const word = document.querySelector("#searchText").value;
+//   fetch("http://localhost:8080/search", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ word: word }),
+//   })
+//     .then((res) => res.json())
+//     .then((json) => {
+//       imgContainer.innerHTML = "";
+//       const images = json.images;
+//       console.log(images);
+//       // imgContainer.innerHTML = "";
+//       for (let i = 0; i < images.length; i++) {
+//         if (images[i] === null) {
+//           continue;
+//         }
+//         const new_pTag1 = document.createElement("div");
+//         new_pTag1.setAttribute("class", "movie1");
+//         const new_pTag2 = document.createElement("img");
+//         new_pTag2.setAttribute("class", "imgs");
+//         new_pTag2.setAttribute("src", `${images[i]}`);
+//         new_pTag2.setAttribute("alt", "movie1");
+//         new_pTag1.appendChild(new_pTag2);
+//         imgContainer.appendChild(new_pTag1);
+//       }
+//     });
+// });
 
-const filterClickBtn = document.querySelector("#click2");
+// const filterClickBtn = document.querySelector("#click2");
